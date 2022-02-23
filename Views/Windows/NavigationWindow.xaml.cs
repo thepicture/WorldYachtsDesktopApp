f8da13151ad048225612178f2719935cce453a8f
thepicture <85333076+thepicture@users.noreply.github.com>
2022-02-23 08:36:06 +0000

    feat(design): create styles for the app

---
+++ Views/Windows/NavigationWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WorldYachtsDesktopApp
+{
+    /// <summary>
+    /// Interaction logic for NavigationWindow.xaml
+    /// </summary>
+    public partial class NavigationWindow : Window
+    {
+        public NavigationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
