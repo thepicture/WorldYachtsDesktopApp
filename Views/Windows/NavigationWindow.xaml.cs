663e9fc4fc45ede93e8b6845b221fd54fee98353
thepicture <85333076+thepicture@users.noreply.github.com>
2022-02-27 07:22:58 +0000

    feat: add ability to log out

--- Views/Windows/NavigationWindow.xaml.cs
+++ Views/Windows/NavigationWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Navigation;
+using WorldYachtsDesktopApp.Services;
 using WorldYachtsDesktopApp.Views.Pages;
 
 namespace WorldYachtsDesktopApp
@@ -8,6 +11,8 @@ namespace WorldYachtsDesktopApp
     /// </summary>
     public partial class NavigationWindow : Window
     {
+        private readonly IFeedbackService feedbackService =
+            new MessageBoxFeedbackService();
         public NavigationWindow()
         {
             InitializeComponent();
@@ -20,6 +25,45 @@ namespace WorldYachtsDesktopApp
         private void PerformGoBack(object sender, RoutedEventArgs e)
         {
             MainFrame.GoBack();
+        }
+
+        /// <summary>
+        /// Завершить сессию.
+        /// </summary>
+        private async void PerformGoToLoginPage(object sender,
+                                                RoutedEventArgs e)
+        {
+            if (!await feedbackService
+                .AskAsync("Завершить сессию и перейти на окно авторизации?"))
+            {
+                return;
+            }
+            (App.Current as App).User = null;
+            while (MainFrame.CanGoBack)
+            {
+                MainFrame.GoBack();
+            }
+        }
+
+        /// <summary>
+        /// Вызывается в момент окончания навигации.
+        /// </summary>
+        private void OnNavigated(object sender, NavigationEventArgs e)
+        {
+            if ((App.Current as App).User is null)
+            {
+                if (MainFrame.CanGoBack)
+                    GoBack.Visibility = Visibility.Visible;
+                else
+                    GoBack.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                if (MainFrame.BackStack.Cast<JournalEntry>().Count() < 3)
+                    GoBack.Visibility = Visibility.Collapsed;
+                else
+                    GoBack.Visibility = Visibility.Visible;
+            }
         }
     }
 }
