b5a86e5a59a9338db555e89edb3f726b0b493074
thepicture <85333076+thepicture@users.noreply.github.com>
2022-02-27 08:03:04 +0000

    feat(userspage): add the ability to delete an user

--- Views/Pages/AdminPages/UsersPage.xaml.cs
+++ Views/Pages/AdminPages/UsersPage.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using WorldYachtsDesktopApp.Models.Entities;
+using WorldYachtsDesktopApp.Services;
 
 namespace WorldYachtsDesktopApp.Views.Pages.AdminPages
 {
@@ -12,6 +15,8 @@ namespace WorldYachtsDesktopApp.Views.Pages.AdminPages
     /// </summary>
     public partial class UsersPage : Page
     {
+        private readonly IFeedbackService feedbackService =
+            new MessageBoxFeedbackService();
         public UsersPage()
         {
             InitializeComponent();
@@ -23,6 +28,15 @@ namespace WorldYachtsDesktopApp.Views.Pages.AdminPages
         /// </summary>
         private async void OnLoaded(object sender, RoutedEventArgs e)
         {
+            await LoadUsers();
+        }
+
+        /// <summary>
+        /// Подгружает список пользователей.
+        /// </summary>
+        /// <returns></returns>
+        private async Task LoadUsers()
+        {
             UsersGrid.ItemsSource = await Task.Run(() =>
             {
                 using (WorldYachtsBaseEntities context =
@@ -30,9 +44,40 @@ namespace WorldYachtsDesktopApp.Views.Pages.AdminPages
                 {
                     return context.User
                     .Include(u => u.Role)
+                    .Where(u => !u.IsDeleted)
                     .ToList();
                 }
             });
+        }
+
+        private async void OnUserDelete(object sender, RoutedEventArgs e)
+        {
+            User user = (sender as Button).DataContext as User;
+            if (!await feedbackService
+                .AskAsync($"Удалить пользователя {user.Login}?"))
+            {
+                return;
+            }
+
+            try
+            {
+                await Task.Run(() =>
+                {
+                    using (WorldYachtsBaseEntities context =
+                    new WorldYachtsBaseEntities())
+                    {
+                        context.User.Find(user.UserId).IsDeleted = true;
+                        context.SaveChanges();
+                    }
+                });
+                await feedbackService
+                    .InformAsync($"Пользователь {user.Login} удалён");
+                await LoadUsers();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.StackTrace);
+            }
         }
     }
 }
