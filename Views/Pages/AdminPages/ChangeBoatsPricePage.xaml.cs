c18856f24c3837a60835aab3a2c5054eaef5b895
thepicture <85333076+thepicture@users.noreply.github.com>
2022-03-01 15:28:42 +0000

    feats(boatspage): add the price change logic

--- Views/Pages/AdminPages/ChangeBoatsPricePage.xaml.cs
+++ Views/Pages/AdminPages/ChangeBoatsPricePage.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using WorldYachtsDesktopApp.Models.Entities;
+using WorldYachtsDesktopApp.Services;
 
 namespace WorldYachtsDesktopApp.Views.Pages.AdminPages
 {
@@ -9,6 +14,8 @@ namespace WorldYachtsDesktopApp.Views.Pages.AdminPages
     /// </summary>
     public partial class ChangeBoatsPricePage : Page
     {
+        readonly IFeedbackService feedbackService =
+            new MessageBoxFeedbackService();
         public ChangeBoatsPricePage(IEnumerable<Boat> boats)
         {
             InitializeComponent();
@@ -16,5 +23,101 @@ namespace WorldYachtsDesktopApp.Views.Pages.AdminPages
         }
 
         public IEnumerable<Boat> Boats { get; set; }
+
+        /// <summary>
+        /// Изменяет цену лодок.
+        /// </summary>
+        private async void PerformPriceChange(object sender, RoutedEventArgs e)
+        {
+            string action = (PriceActions.SelectedItem as ComboBoxItem)
+                .Content
+                .ToString();
+            switch (action)
+            {
+                case "увеличить":
+                    try
+                    {
+                        double howMuchPercent =
+                            Convert.ToInt32(HowMuchBox.Text)
+                            * 1.0
+                            / 100;
+                        await Task.Run(() =>
+                        {
+                            using (WorldYachtsBaseEntities context =
+                            new WorldYachtsBaseEntities())
+                            {
+                                foreach (Boat boat in Boats)
+                                {
+                                    context.Boat
+                                    .Find(boat.BoatId)
+                                    .BasePrice += Convert
+                                    .ToInt32(
+                                        Math.Ceiling(
+                                            howMuchPercent
+                                        * Convert.ToDouble(boat.BasePrice)
+                                        )
+                                        );
+                                }
+                                context.SaveChanges();
+                            }
+                        });
+                        await feedbackService.InformAsync("Цена успешно изменена");
+                        NavigationService.GoBack();
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.ToString());
+                        await feedbackService
+                            .InformErrorAsync("Не удалось "
+                                              + "изменить цену. "
+                                              + "Попробуйте "
+                                              + "перезагрузить страницу "
+                                              + "и попробовать ещё раз");
+                    }
+                    break;
+                case "уменьшить":
+                    try
+                    {
+                        double howMuchPercent =
+                           Convert.ToInt32(HowMuchBox.Text)
+                           * 1.0
+                           / 100;
+                        await Task.Run(() =>
+                        {
+                            using (WorldYachtsBaseEntities context =
+                            new WorldYachtsBaseEntities())
+                            {
+                                foreach (Boat boat in Boats)
+                                {
+                                    context.Boat
+                                    .Find(boat.BoatId)
+                                    .BasePrice -= Convert
+                                    .ToInt32(
+                                        Math.Ceiling(
+                                            howMuchPercent
+                                        * Convert.ToDouble(boat.BasePrice)
+                                        ));
+                                }
+                                context.SaveChanges();
+                            }
+                        });
+                        await feedbackService.InformAsync("Цена успешно изменена");
+                        NavigationService.GoBack();
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.ToString());
+                        await feedbackService
+                            .InformErrorAsync("Не удалось "
+                                              + "изменить цену. "
+                                              + "Попробуйте "
+                                              + "перезагрузить страницу "
+                                              + "и попробовать ещё раз");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
